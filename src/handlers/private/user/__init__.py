1775bd53ba251bf5f73905d0da8b9dfdeaa595e7
umarr <umarr.developer@gmail.com>
2024-10-13 21:23:54 +0600

    include weather router to user router

--- src/handlers/private/user/__init__.py
+++ src/handlers/private/user/__init__.py
@@ -1,7 +1,9 @@
 from aiogram import Router
 
 from src.handlers.private.user.start import router as start_router
+from src.handlers.private.user.weather import router as weather_router
 
 router = Router()
 
 router.include_router(start_router)
+router.include_router(weather_router)
